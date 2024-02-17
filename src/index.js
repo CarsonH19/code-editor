2f995288ac6bdfed58f10134e9998a4f517ebf03
CarsonH19 <questline.carson@gmail.com>
2024-02-17 22:55:34 +0900

    fix: bugs

--- src/index.js
+++ src/index.js
@@ -1,48 +1,58 @@
-import './style.css';
+// import CodeMirror from 'codemirror';
+// import 'codemirror/lib/codemirror.css'; // Import CodeMirror CSS
+// import 'codemirror/theme/material.css'; // Import a CodeMirror theme
+// import 'codemirror/mode/javascript/javascript'; // Import a CodeMirror mode (optional)
+
+import "./style.css";
 
 // Catching commonly used elements to minimize dom queries
-const livePreviewFrame = document.getElementById('live-preview');
-const htmlEditor = document.getElementById('html');
-const cssEditor = document.getElementById('css');
-const jsEditor = document.getElementById('js');
+const livePreviewFrame = document.getElementById("live-preview");
+const htmlEditor = document.getElementById("html");
+const cssEditor = document.getElementById("css");
+const jsEditor = document.getElementById("js");
 
 // Function to set up the live preview iframe and include necessary scripts
 function initializeLivePreview() {
-  livePreviewFrame.contentWindow.document.body.innerHTML = '';
-  const styleElement = document.createElement('style');
-  styleElement.setAttribute('id', 'live-preview-style');
-  livePreviewFrame.contentWindow.document.head .appendChild(styleElement);
-  const pagedJsScript = document.createElement('script');
-  pagedJsScript.src = 'https://unpkg.com/pagedjs/dist/paged.legacy.polyfill.js';
-  livePreviewFrame.contentWindow.document.head.appendChild(pagedJsScript); 
+  livePreviewFrame.contentWindow.document.body.innerHTML = "";
+  const styleElement = document.createElement("style");
+  styleElement.setAttribute("id", "live-preview-style");
+  livePreviewFrame.contentWindow.document.head.appendChild(styleElement);
+
+  const pagedJsScript = document.createElement("script");
+  pagedJsScript.src = "https://unpkg.com/pagedjs/dist/paged.legacy.polyfill.js";
+  livePreviewFrame.contentWindow.document.head.appendChild(pagedJsScript);
 }
 
 // Function to update the live preview iframe with the html code from editor
 function updateLiveHTMLPreview(codeEditors) {
-  livePreviewFrame.contentWindow.document.body.innerHTML = codeEditors.html.getValue();
+  livePreviewFrame.contentWindow.document.body.innerHTML =
+    codeEditors.html.getValue();
 }
 
-// Function to update the live preview iframe with the css code from editor 
+// Function to update the live preview iframe with the css code from editor
 function updateLiveCSSPreview(codeEditors) {
-  const styleElement = livePreviewFrame.contentWindow.document.getElementById('live-preview-style');
+  const styleElement =
+    livePreviewFrame.contentWindow.document.getElementById(
+      "live-preview-style"
+    );
   styleElement.innerHTML = codeEditors.css.getValue();
 }
 
-// Function to update the live preview iframe with the js code from editor 
+// Function to update the live preview iframe with the js code from editor
 function updateLiveJSPreview(codeEditors) {
-  const scriptElement = document.createElement('script');
+  const scriptElement = document.createElement("script");
   scriptElement.innerHTML = codeEditors.js.getValue();
   livePreviewFrame.contentWindow.document.body.appendChild(scriptElement);
 }
 
-// Function to initialize CodeMirror editor for html,css, and js
+// Function to initialize CodeMirror editors for html, css and javascript
 function initializeCodeEditors() {
   function getDefaultOptions(object) {
     const defaultOptions = {
-      lineNumber: true,
+      lineNumbers: true,
       autoCloseTags: true,
       autoCloseBrackets: true,
-      theme: 'panda-syntax'
+      theme: "panda-syntax",
     };
     if (object) {
       const keys = Object.keys(object);
@@ -54,18 +64,32 @@ function initializeCodeEditors() {
   }
 
   const codeEditors = {
-    html: CodeMirror(htmlEditor, getDefaultOptions({
-      mode: 'text/html',
-      value: '',
-    })),
-    css: CodeMirror(cssEditor, getDefaultOptions({
-      mode: 'css',
-      value: '',
-    })),
-    js: CodeMirror(jsEditor, getDefaultOptions({
-      mode: 'javascript',
-      value: '',
-    })),
+    html: CodeMirror(
+      htmlEditor,
+      getDefaultOptions({
+        mode: "text/html",
+        value: "",
+      })
+    ),
+    css: CodeMirror(
+      cssEditor,
+      getDefaultOptions({
+        mode: "css",
+        value: "",
+        extraKeys: { "Ctrl-Space": "autocomplete" },
+        hintOptions: {
+          completeSingle: false,
+          closeOnUnfocus: false,
+        },
+      })
+    ),
+    js: CodeMirror(
+      jsEditor,
+      getDefaultOptions({
+        mode: "javascript",
+        value: "",
+      })
+    ),
   };
   return codeEditors;
 }
@@ -74,24 +98,24 @@ function initializeCodeEditors() {
 function setupLivePreviewStudio() {
   const codeEditors = initializeCodeEditors();
 
-  // Event Listener for changes in HTML editor
-  CodeMirror.on(codeEditors.html, 'change', () => {
+  // Event listener for changes in HTML editor
+  CodeMirror.on(codeEditors.html, "change", () => {
     updateLiveHTMLPreview(codeEditors);
   });
 
-  // Event Listener for changes in CSS editor
-  CodeMirror.on(codeEditors.css, 'change', () => {
+  // Event listener for changes in CSS editor
+  CodeMirror.on(codeEditors.css, "change", () => {
     updateLiveCSSPreview(codeEditors);
   });
 
-  // Event Listener for changes in JavaScript editor
-  CodeMirror.on(codeEditors.js, 'change', () => {
+  // Event listener for changes in HTML editor
+  CodeMirror.on(codeEditors.js, "change", () => {
     updateLiveJSPreview(codeEditors);
   });
 }
 
 // Event listener to set up the live preview studio after the dom is fully loaded
-document.addEventListener('DOMContentLoaded', () => {
-  initializeCodeEditors();
+document.addEventListener("DOMContentLoaded", () => {
+  initializeLivePreview();
   setupLivePreviewStudio();
 });
