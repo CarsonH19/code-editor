32ae3db4516f9ab6cf8a9f91992b8615f11e09a1
CarsonH19 <questline.carson@gmail.com>
2024-02-17 22:25:18 +0900

    feat: updateLivePreview & initializeCodeEditors

--- src/index.js
+++ src/index.js
@@ -18,3 +18,54 @@ function initializeLivePreview() {
 }
 
 // Function to update the live preview iframe with the html code from editor
+function updateLiveHTMLPreview(codeEditors) {
+  livePreviewFrame.contentWindow.document.body.innerHTML = codeEditors.html.getValue();
+}
+
+// Function to update the live preview iframe with the css code from editor 
+function updateLiveCSSPreview(codeEditors) {
+  const styleElement = livePreviewFrame.contentWindow.document.getElementById('live-preview-style');
+  styleElement.innerHTML = codeEditors.css.getValue();
+}
+
+// Function to update the live preview iframe with the js code from editor 
+function updateLiveJSPreview(codeEditors) {
+  const scriptElement = document.createElement('script');
+  scriptElement.innerHTML = codeEditors.js.getValue();
+  livePreviewFrame.contentWindow.document.body.appendChild(scriptElement);
+}
+
+// Function to initialize CodeMirror editor for html,css, and js
+function initializeCodeEditors() {
+  function getDefaultOptions(object) {
+    const defaultOptions = {
+      lineNumber: true,
+      autoCloseTags: true,
+      autoCloseBrackets: true,
+      theme: 'panda-syntax'
+    };
+    if (object) {
+      const keys = Object.keys(object);
+      for (const key of keys) {
+        defaultOptions[key] = object[key];
+      }
+    }
+    return defaultOptions;
+  }
+
+  const codeEditors = {
+    html: CodeMirror(htmlEditor, getDefaultOptions({
+      mode: 'text/html',
+      value: '',
+    })),
+    css: CodeMirror(cssEditor, getDefaultOptions({
+      mode: 'css',
+      value: '',
+    })),
+    js: CodeMirror(jsEditor, getDefaultOptions({
+      mode: 'javascript',
+      value: '',
+    })),
+  };
+  return codeEditors;
+}
